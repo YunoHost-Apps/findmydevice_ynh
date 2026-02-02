039f2c76a15ab1570dc75abb3ef2beb0076e28de
ewilly <ewilly@free.fr>
2026-02-02 15:27:46 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,7 +17,7 @@ myynh_build() {
 		pushd "$install_dir/source/web"
 			ynh_hide_warnings corepack enable && corepack prepare pnpm@latest --activate
 			ynh_hide_warnings ynh_exec_as_app pnpm install
-			ynh_hide_warnings ynh_exec_as_app pnpm build --no-color --no-progress --reporter=append-only
+			ynh_hide_warnings ynh_exec_as_app CI=true NO_COLOR=1 pnpm build
 		popd
 
 	# Compile the Go code into a static Go binary
